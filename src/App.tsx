9fa874462fdf3db7961295c903fe2c9f1a539e5b
Anthony Parrino <anthonyparrino@Anthonys-Air.attlocal.net>
2022-12-31 21:42:02 -0500

    created vite app

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import UserProfile from './pages/UserProfile';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
